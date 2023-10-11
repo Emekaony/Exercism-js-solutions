fbeab1ff31368e1073a4bb15b387f4b83a930c51
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-10-10 19:48:25 -0700

    Figured it out

--- poetry-club-door-policy/door-policy.js
+++ poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line[0];
 }
 
 /**
@@ -38,7 +38,8 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  word = word.toLowerCase();
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 /**
@@ -49,7 +50,9 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  line = line.trim();
+  let l = line.length;
+  return line[l - 1];
 }
 
 /**
@@ -60,5 +63,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return frontDoorPassword(word) + ", please";
 }
